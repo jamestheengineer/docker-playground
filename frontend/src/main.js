46a9c67ee01503202a983199a3e6044e358c2d2c
jamestheengineer <jamestheengineer@gmail.com>
2020-05-21 18:17:07 -0400

    Added element UI and axios libraries

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue';
+import ElementUI from 'element-ui';
 import App from './App.vue';
 import './registerServiceWorker';
 import router from './router';
 import store from './store';
+
+Vue.use(ElementUI);
 
 Vue.config.productionTip = false;
 
